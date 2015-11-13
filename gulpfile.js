4a8e0c5764ca65adcd271eb07ef54e7687090178
James Howell <james.howell@maynardmalone.com>
2015-11-13 17:30:10 +0000

    Updated ignore file - ignoring final build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-var gulp = require('gulp');
+var
+	gulp = require('gulp'),
+	sass = require('gulp-ruby-sass'),
+	sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass', function () {
+  return sass('scss/app.scss', { sourcemap: true })
+    .on('error', sass.logError)
+
+    // For inline sourcemaps
+    .pipe(sourcemaps.write())
+
+    // For file sourcemaps
+    .pipe(sourcemaps.write('maps', {
+      includeContent: false,
+      sourceRoot: 'scss'
+    }))
+
+    .pipe(gulp.dest('dist/css'));
 });
+
+
+gulp.task('default', ['sass']);
