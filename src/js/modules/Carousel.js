932169e6540cc1c09473f833ade83937b978c52d
James Howell <james@decodedcreative.com>
2016-12-12 18:47:23 +0000

    es6 carousel

--- src/js/modules/Carousel.js
+++ src/js/modules/Carousel.js
@@ -1,68 +1,120 @@
 class Carousel {
-	
 	// set up instance variables
-
 	constructor (options) {
 		
 		this.element = options.element;
 		this.carousel = options.element.querySelectorAll('ul');
+		this.carouselNav = options.element.querySelectorAll('button');
 		this.carouselSlides = this.carousel[0].children;
 		this.carouselHeight = 0;
-
+		self = this;
 	}
 
 	resize () {
 
-		// Get tallest slide
-
-		Array.prototype.map.call( this.carouselSlides, ( slide ) => {
-			this.carouselHeight = (slide.offsetHeight > this.carouselHeight) ? slide.offsetHeight : this.carouselHeight;
+		Array.prototype.map.call( self.carouselSlides, ( slide ) => {
+			self.carouselHeight = (slide.offsetHeight > self.carouselHeight) ? slide.offsetHeight : self.carouselHeight;
 		});
 
 
+		self.carousel[0].style.removeProperty('height');
 
 		//Set the height of the carousel to the height of its tallest slide
+		self.carousel[0].style.height = self.carouselHeight+'px';
 
-		this.carousel[0].style.height = this.carouselHeight+'px';
-
-
-		
-		//Reset the height of the carousel to zero
-
-		this.carouselHeight = 0;
+		self.carouselHeight = 0;
 
 	}
+	
+	
+	getActiveSlide () {
+		
+		let activeSlide;
+		
+		//Get currently active slide 
+		for (let slide of self.carousel[0].children){
+			
+			if ( slide.classList.contains('active') === true ) {
+				activeSlide = slide;
+			}
+		}
+		
+		return activeSlide;
+		
+	}
+	
+	changeSlide () {
+		
+		let activeSlide = self.getActiveSlide(),
+			direction = null,
+			lastSlide = null,
+			firstSlide = null;
+		
+		for ( let slide of self.carouselSlides){
+			slide.classList.remove('active');
+		}
 
+		if ( this.classList.contains('prev') ) {
+			direction = "previous";
+		} else if ( this.classList.contains('next') ){
+			direction = "next";
+		}
+
+		
+		if( direction === 'previous' ) {
+
+			if( activeSlide.previousElementSibling !== null){
+				
+				activeSlide.previousElementSibling.classList.add('active');
+				
+			} else {
+				
+				lastSlide = self.carousel[0].children[self.carousel[0].children.length - 1];
+				lastSlide.classList.add('active');
+				
+			}
+			
+		} else if (direction === 'next' ) {
+			
+			if( activeSlide.nextElementSibling !== null){
+				
+				activeSlide.nextElementSibling.classList.add('active');
+				
+			} else {
+				
+				firstSlide = self.carousel[0].children[0];
+				firstSlide.classList.add('active');
+				
+			}
+
+		}
+		
+
+		
+	}
 
 	// initial set up
-
 	setup () {
 		
-		// Carousel shows as list of images without JS. With JS enabled (on load) add 'loaded' CSS class to carousel to invoke carousel styles
-		
+		// Run the resize function to give the carousel the height of its tallest slide
+		self.resize();
+
 		this.element.classList.add("loaded");
-
-
-
-		// Add active class to the first slide in the carousel
-
-		this.carousel[0].firstElementChild.classList.add("active");
-
-
-		// Run resize function which calculates the height of the tallest slide in the carousel and sets the height of the carousel to this value
 		
-		this.resize();
+		// Add an active class to the first slide
+		self.carouselSlides[0].classList.add('active');
+		
+		// On resize adjust the height of the carousel
+		window.onresize = this.resize.bind(this);
+		
+		for (let button of self.carouselNav){
+			button.addEventListener('click', self.changeSlide);
+		}
+		
 
-
-		// When the browser is resized, run the resize function
-
-		window.addEventListener("resize", this.resize.bind(this));
-	
 	}
-
-
-
 }
+
 
 // Private methods
 // --------------------
